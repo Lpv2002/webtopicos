04356cdf3770095f92a524f27a0a3db0afee9737
Fabio Andre Arredondo Mendieta <fabioarredondo44@gmail.com>
2024-04-28 20:25:39 -0400

    first commit

--- src/routes/inicio.js
+++ src/routes/inicio.js
@@ -1,30 +1,52 @@
-import { Router } from "express" 
-import{Invitar, SendData, cerrarSesion, crearPartida, crearPartidaForm,  register, renderHome, renderInvitar, renderPartidas, renderPerfil, renderlogin} from "../controllers/index.CO.js"
+import { Router } from "express"
+import { Invitar, SendData, cerrarSesion, crearPartida, crearPartidaForm, register, renderHome, renderInvitar, renderPartidas, renderPerfil, renderlogin } from "../controllers/index.CO.js"
+import multer from 'multer';
+import FormData from 'form-data';
+import Dir from "../routes/api-local.js";
+import axios from 'axios';
 
 
 const Inicio = Router();
 
 Inicio.get('/', renderHome)
 
-Inicio.get('/login',renderlogin)
+Inicio.get('/login', renderlogin)
 
-Inicio.post('/login',SendData)
+Inicio.post('/login', SendData)
 
 Inicio.get('/cerrar', cerrarSesion)
 
-Inicio.post('/register',register)
+Inicio.post('/register', register)
 
-Inicio.get('/perfil',renderPerfil)
+Inicio.get('/perfil', renderPerfil)
 
-Inicio.get('/partidas',renderPartidas)
+Inicio.get('/partidas', renderPartidas)
 
-Inicio.get('/crearPartida',crearPartida)
+Inicio.get('/crearPartida', crearPartida)
 
-Inicio.get('/invitar',renderInvitar)
+Inicio.get('/invitar', renderInvitar)
 
-Inicio.post('/invitar',Invitar)
+Inicio.post('/invitar', Invitar)
 
-Inicio.post('/crearPartida',crearPartidaForm)
+Inicio.post('/crearPartida', crearPartidaForm)
+
+Inicio.post('/Iniciar', Invitar)
+const upload = multer({ storage: multer.memoryStorage() });
+Inicio.post('/Update', upload.single('qr'),async (req, res) => {
+    const form = new FormData();
+    form.append('qr', req.file.buffer, { filename: req.file.originalname, contentType: req.file.mimetype });
+
+    try {
+        const response = await axios.post(Dir + "/api/subirQr/" + req.user.usser, form, {
+            headers: form.getHeaders()
+        });
+
+        res.redirect("/partida")
+    } catch (error) {
+        console.error(error);
+        res.send('Error al enviar la imagen');
+    }
+})
 
 
 export default Inicio;
